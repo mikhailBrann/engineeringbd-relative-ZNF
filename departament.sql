81b2c345e87c1b42709bf0fcdca6a718fc676f6e
mikhailBrann <alunos13@yandex.ru>
2021-11-29 14:23:47 +0300

    changed to work

--- departament.sql
+++ departament.sql
@@ -1,23 +1,14 @@
-create table if not exists Managers (
-	id serial primary key
+create table if not exists Workers (
+	id serial primary key,
+	manager_id integer references Workers(id),
+	worker_name varchar(80) not null
 );
 
 create table if not exists Departaments (
 	id serial primary key,
-	title varchar(80) not null unique
+	title varchar(90) not null unique,
+	manager_id integer references Workers(id),
+	worker_id integer references Workers(id)
 );
 
-create table if not exists Workers (
-	id serial primary key,
-	worker_name varchar(80) not null
-);
-
-
-alter table Managers add departament_id integer references Departaments(id);
-alter table Managers add worker_id integer references Workers(id);
-
-alter table Departaments add manager_id integer references Managers(id);
-alter table Departaments add worker_id integer references Workers(id);
-
-alter table Workers add manager_id integer references Managers(id);
 alter table Workers add departament_id integer references Departaments(id);
--- music_store.sql
+++ music_store.sql
@@ -1,7 +1,16 @@
-create table if not exists Tracks (
+create table if not exists Genre (
 	id serial primary key,
-	title varchar(100) not null,
-	track_length varchar(9) not null
+	title varchar(100) not null
+);
+
+create table if not exists Authors (
+	id serial primary key,
+	author_name varchar(100) not null
+);
+
+create table if not exists GenreAndAuthors (
+	genre_id integer references Genre(id),
+	author_id integer references Authors(id)
 );
 
 create table if not exists Albums (
@@ -9,33 +18,24 @@ create table if not exists Albums (
 	title varchar(100) not null
 );
 
-create table if not exists Authors (
-	id serial primary key,
-	author_name varchar(100) not null	
-);
-
-create table if not exists Genre (
-	id serial primary key,
-	title varchar(100) not null
+create table if not exists AutorsAndAlbums (
+	author_id integer references Authors(id),
+	album_id integer references Albums(id)
 );
 
 create table if not exists Collections (
 	id serial primary key,
-	title varchar(100) not null	
+	title varchar(100) not null
 );
 
+create table if not exists Tracks (
+	id serial primary key,
+	title varchar(100) not null,
+	album_id integer references Albums(id),
+	track_length varchar(30) not null
+);
 
-alter table Tracks add genre_id integer references Genre(id);
-alter table Tracks add album_id integer references Albums(id);
-alter table Tracks add collection_id integer references Collections(id);
-
-alter table Albums add author_id integer references Authors(id);
-alter table Albums add genre_id integer references Genre(id);
-
-alter table Authors add album_id integer references Albums(id);
-alter table Authors add genre_id integer references Genre(id);
-
-alter table Genre add author_id integer references Authors(id);
-alter table Genre add album_id integer references Albums(id);
-
-alter table Collections add track_id integer references Tracks(id);
+create table if not exists CollectAndTracks (
+	collection_id integer references Collections(id),
+	track_id integer references Tracks(id)
+);
