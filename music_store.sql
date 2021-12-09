155028b438f4c6b29f1ed0866aa32eaae31d6a8a
mikhailBrann <alunos13@yandex.ru>
2021-12-09 14:10:11 +0300

    correct work

--- music_store.sql
+++ music_store.sql
@@ -9,23 +9,27 @@ create table if not exists Authors (
 );
 
 create table if not exists GenreAndAuthors (
+	id serial primary key,
 	genre_id integer references Genre(id),
 	author_id integer references Authors(id)
 );
 
 create table if not exists Albums (
 	id serial primary key,
-	title varchar(100) not null
+	title varchar(100) not null,
+	album_year varchar(20) not null
 );
 
 create table if not exists AutorsAndAlbums (
+	id serial primary key,
 	author_id integer references Authors(id),
 	album_id integer references Albums(id)
 );
 
 create table if not exists Collections (
 	id serial primary key,
-	title varchar(100) not null
+	title varchar(100) not null,
+	collection_year varchar(20) not null
 );
 
 create table if not exists Tracks (
@@ -36,6 +40,7 @@ create table if not exists Tracks (
 );
 
 create table if not exists CollectAndTracks (
+	id serial primary key,
 	collection_id integer references Collections(id),
 	track_id integer references Tracks(id)
 );
